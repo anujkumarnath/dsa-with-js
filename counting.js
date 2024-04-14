b23ad2eca795bd9b82c289979d5a6853f87b9bf5
Anuj <anujkumarnath@gmail.com>
2024-04-14 07:54:43 +0000

    counting sort

---
+++ counting.js
@@ -0,0 +1,28 @@
+const arr = [5, 4, 5, 3, 2, 5, 0, 1, 0, 0, 3, 8, 4, 3, 3, 2, 1, 1, 5];
+
+console.log('unsorted:', arr.toString());
+
+/* find larges element */
+let largest = arr[0];
+for (let i = 1; i < arr.length; i++) {
+	if (arr[i] > largest) {
+		largest = arr[i];
+	}
+}
+
+/* create counts array with index 0, 1, ..., largest */
+const counts = Array(largest + 1).fill(0);
+for (let i = 0; i < arr.length; i++) {
+	const num = arr[i];
+	counts[num] = counts[num] + 1;
+}
+
+/* create sorted arrray */
+const sorted = [];
+for (let i = 0; i < counts.length; i++) {
+	for (let j = 1; j <= counts[i]; j++) {
+		sorted.push(i);
+	}
+}
+
+console.log('sorted  :', sorted.toString());
